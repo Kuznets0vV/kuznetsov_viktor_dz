cb13a15f680f7eeed659c39625b8ac5cd23383c3
Kuznets0vV <103839728+Kuznets0vV@users.noreply.github.com>
2022-06-01 19:00:50 +0300

    добавил оптимизированный вариант

--- task_5_5.py
+++ task_5_5.py
@@ -1,15 +1,12 @@
 import time
 
-'''результат в лоб'''
-
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+
 start = time.perf_counter()
 result = []
-[result.append(i) for i in src if src.count(i) == 1]
+[result.append(i) for i in src if src.count(i) == 1] # медленный вариант
 end = time.perf_counter()
 print(result, f'время исполнения {end - start} s')
-
-'''оптимизированный результат'''
 
 start = time.perf_counter()
 res = set()
@@ -20,6 +17,6 @@ for el in src:
     else:
         res.discard(el)
     tmp.add(el)
-result_opti = [el for el in src if el in res]
+result_opti = [el for el in src if el in res] # быстрый вариант
 end = time.perf_counter()
 print(result_opti, f'время исполнения {end - start} s')
