8efa3a07959f6a15440b598ca9b85d6b45012373
Kuznets0vV <103839728+Kuznets0vV@users.noreply.github.com>
2022-06-16 16:48:42 +0300

    1

---
+++ task_8_3.py
@@ -0,0 +1,20 @@
+from functools import wraps
+def type_logger(func):
+    @wraps(func)
+    def wrapper(*args):
+        for arg in args:
+            print(f'{func.__name__}({arg}: {type(arg)})', end=', ')
+        return func(*args)
+
+    return wrapper
+
+
+@type_logger
+def calc_cube(*args):
+    return list(map(lambda x: x ** 3, args))
+
+
+a = calc_cube(5, 7)
+print(a)
+print(calc_cube.__name__)
+
